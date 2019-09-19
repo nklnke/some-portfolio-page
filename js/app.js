51bccd4a212db7de18ab344599a70c0fb1fd4c99
Danil Kozlov <megacokes@gmail.com>
2019-09-19 12:40:53 +0500

    Add modals behavior

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 $(function() {
-  // Фильтр по тегам
+  // Filter by tags
   let filter = $("[data-filter]");
 
   filter.on("click", function(event) {
@@ -20,5 +20,61 @@ $(function() {
         }
       });
     }
+  });
+
+  // Modals
+  //
+  let modalOpen = $("[data-modal]");
+  let modalClose = $("[data-close]");
+
+  modalOpen.on("click", function(event) {
+    event.preventDefault();
+    let modalId = $(this).data("modal");
+    $(modalId).addClass("show");
+    $("body").addClass("no-scroll");
+
+    // Animation
+    setTimeout(function() {
+      $(modalId)
+        .find(".modal__dialog")
+        .css({
+          transform: "rotateX(0)"
+        });
+    }, 100);
+  });
+
+  modalClose.on("click", function(event) {
+    let modalParent = $(this).parents(".modal");
+    event.preventDefault();
+
+    // Animation
+    modalParent.find(".modal__dialog").css({
+      transform: "rotateX(90deg)"
+    });
+
+    setTimeout(function() {
+      modalParent.removeClass("show");
+      $("body").removeClass("no-scroll");
+    }, 100);
+  });
+
+  // Close modal by clicking on the empty space
+  $(".modal").on("click", function() {
+    let $this = $(this); // Фигня какая-то. Глупая конструкция, но не могу понять, как без неё в данном случае
+
+    $this.find(".modal__dialog").css({
+      transform: "rotateX(90deg)"
+    });
+
+    setTimeout(function() {
+      $this.removeClass("show");
+      $("body").removeClass("no-scroll");
+    }, 100);
+  });
+
+  // stopPropagation - отмена события при клике по родителю окна
+  // (чтобы окно не скрывалось при клике на него)
+  $(".modal__dialog").on("click", function(event) {
+    event.stopPropagation();
   });
 });
